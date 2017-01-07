245a4c709122c11e9e3300b58b29e4ceb13c9075
Deepak V <Deepak V>
2017-01-06 19:38:27 -0500

    DV

--- src/main/java/com/wnycl/dao/TeamDaoImpl.java
+++ src/main/java/com/wnycl/dao/TeamDaoImpl.java
@@ -20,7 +20,6 @@ public class TeamDaoImpl extends AbstractDao<Integer, Team> implements TeamDao {
 
 	@Override
 	public Team findByName(String name) {
-		
 		// TODO Auto-generated method stub
 		Criteria crit = createEntityCriteria();
 		crit.add(Restrictions.eq("name", name));
